997f8abf9a5735b6dfe1ef29593a53377bade744
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2015-02-02 20:12:20 +0000

    continued refactoring

--- src/api.py
+++ src/api.py
@@ -23,40 +23,19 @@ import urllib2
 API makes all HTTP requests
 """
 
-class Query:
+def make_and_fire_request(url, data=None, headers=None, method="GET"):
 
-    def __init__(self, url, data=None, headers=None, method="GET"):
+    if data:
+        data = urllib.urlencode(data)
 
-        self.url = url
-        self.data = data
-        self.headers = headers
-        self.method = method
-
-    def get_json(self):
-        data = {}
-        data["url"] = self.url
-        data["data"] = self.data
-        data["headers"] = self.headers
-        data["method"] = self.method
-        return json.dumps(data)
-
-    def build_query(json_str):
-        query_data = json.loads(json_str)
-        return Query(url=query_data["url"], data=query_data["data"], headers=query_data["headers"], method=query_data["method"])
-
-
-
-def make_and_fire_request(query):
-
-    if query.data:
-        query.data = urllib.urlencode(query.data)
-
-    if query.method is "GET":
-        query.url = query.url + "?" + query.data
-        if query.headers:
-            request = urllib2.Request(query.url, headers=query.headers)
-    elif query.method is "POST":
-        request = urllib2.Request(query.url, query.data, query.headers)
+    if method is "GET":
+        url = url + "?" + data
+        if headers:
+            request = urllib2.Request(url, headers=headers)
+        else:
+            request = urllib2.Request(url)
+    elif method is "POST":
+        request = urllib2.Request(url, data, headers)
 
     try:
         response = urllib2.urlopen(request)
@@ -69,6 +48,4 @@ def make_and_fire_request(query):
 
     raw_data = response.read()
     return json.loads(raw_data)
-
-
 
--- src/twitter_api.py
+++ src/twitter_api.py
@@ -15,14 +15,9 @@
 #   limitations under the License.
 
 
-import json
-import copy
-import base64
-import urllib
-import urllib2
-
-from collections import defaultdict
-from datetime import datetime
+import oauth2 as oauth
+import time
+import urlparse
 
 from api import *
 from _credentials import *
@@ -38,108 +33,34 @@ class Twitter_API:
 
         self.api_base = scheme + api_url + "/" + version
 
-        # seconds between queries to each endpoint
-        query_intervals = {
-            "statuses/lookup": (15 * 60) / 60,               # 60 requests per 15 minute window (app authentication) 
-            "users/lookup": (15 * 60) / 60                    # 60 requests per 15 minute window (app authentication) 
-        }
+        self.client_id = twitter_client_id
+        self.client_secret = twitter_client_secret
+        self.access_token = twitter_access_token
+        self.access_secret = twitter_access_token_secret
 
-        # internal monitoring of time for rate-limiting
-        self.timers = defaultdict(datetime)
-
-        for endpoint, interval in query_intervals.iteritems():
-            self.timers[endpoint] = { "delay": interval,
-                                        "earliest": None}
-
-        # queue of requests to process
-        self.queue = defaultdict(list)
-
-        # authorise the API object to access the API
-        self.acquire_access_token()
+        self.consumer = oauth.Consumer(key=self.client_id, secret=self.client_secret)
+        self.token = oauth.Token(key=self.access_token, secret=self.access_secret)
+        self.client = oauth.Client(self.consumer, self.token)
 
 
-    def acquire_access_token(self):
+    def query(self, url, method="GET", data=None, headers=None):
 
-        client_id = twitter_client_id
-        client_secret = twitter_client_secret
-
-        bearer_credentials = "%s:%s" % (client_id, client_secret)
-        encoded_bearer_credentials = base64.b64encode(bearer_credentials)
-
-        params = {
-            "grant_type": "client_credentials"
-        }
-
-        data = urllib.urlencode(params)
-        url = "https://api.twitter.com/oauth2/token"
-        headers = { "Authorization": "Basic %s" % encoded_bearer_credentials, "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"}
-
-        request = urllib2.Request(url, data, headers)
-
-        try:
-            response = urllib2.urlopen(request)
-        except urllib2.HTTPError, e:
-            raise e
-        except urllib2.URLError, e:
-            raise e
-
-        raw_data = response.read()
-        json_data = json.loads(raw_data)
-
-        self.access_token = json_data["access_token"]
-
-    def build_query(self, endpoint, aspect, in_params=None):
-        #
-        # Params sanitising -- erase any tokens and client credentials...
-        if in_params:
-            params = copy.copy(in_params)
-            for fld in ["access_token","client_id","client_secret"]:
-                if fld in params:
-                    del params[fld]
+        response, content = self.client.request(url, method, body=data, headers=headers)
+        if response['status'] == '200':
+            return content
         else:
-            params = {}
-
-        str_param_data = {}
-        for k, v in params.iteritems():
-            str_param_data[k] = unicode(v).encode("utf-8")
-
-        #
-        # gather parameters for POST request
-        url = self.api_base + "/" + endpoint + "/" + aspect + ".json"
-        data = str_param_data
-        headers = {"Authorization": "Bearer %s" % (self.access_token)}
-
-        q = Query(url, data, headers)  
-        print q.get_json()
-        return q
-
-    def query(self, endpoint, aspect, in_params=None):
-
-        #
-        # make sure we're authorised for querying
-        if not self.access_token:
-            self.acquire_access_token()
-
-        full_endpoint = endpoint + "/" + aspect
-
-
-
-
-        query = self.build_query(endpoint, aspect, in_params)
-
-        #
-        # make the request
-        response = make_and_fire_request(query)
-        print response
-
-    def run(self):
+            return None
 
 
 
 if __name__ == "__main__":
 
     ta = Twitter_API()
-    ta.acquire_access_token()
     params = {"screen_name" : "BBCNews,BBCBreaking,BBCWorld"}
-    ta.query("users", "lookup", params)
+
+    users_lookup_api = ta.api_base + "/" + "users" + "/" + "lookup" + ".json"
+
+    content = ta.query(users_lookup_api, "POST", data=urllib.urlencode(params))
+    print(content)
+
 
